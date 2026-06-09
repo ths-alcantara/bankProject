4b56ab84859855e8b10d952c878bdfc6dc4a9076
Thaís Alcantara <tatie.alcantara@gmail.com>
2026-06-08 21:40:01 -0300

    creating models

---
+++ src/main/java/com/thais/bankProject/model/Feature.java
@@ -0,0 +1,7 @@
+package com.thais.bankProject.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature" )
+public class Feature extends BaseItem{
+}
---
+++ src/main/java/com/thais/bankProject/model/News.java
@@ -0,0 +1,7 @@
+package com.thais.bankProject.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news" )
+public class News extends BaseItem {
+}
